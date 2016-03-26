86cbad2a32a6948e299ab78cde9667f26619bbf3
parag-datar <parag.datar7777@gmail.com>
2016-03-26 17:21:35 -0400

    Format execv

--- kern/syscall/proc_syscall.c
+++ kern/syscall/proc_syscall.c
@@ -187,34 +187,34 @@ sys_execv(const char *program_name, char **args){
     // int padding=0,nargs=0;
     // char curr;
     char** corrected_args = (char **) kmalloc (sizeof(char*));
-    err=copyin((const_userptr_t) args, corrected_args,sizeof(char **));
+    err = copyin((const_userptr_t) args, corrected_args,sizeof(char **));
     if(err){
         return err;
     } 
-    int total=0;
-    for(i=0;args[i]!=NULL;i++){
+    int total = 0;
+    for(i = 0; args[i] != NULL; i++){
         total++;
     }
     int k;
-    int total_length=0;
-    for(k=0;k<total;k++){
+    int total_length = 0;
+    for(k = 0; k < total; k++){
         int len = strlen(args[k]);
-        int padding =0;
-        if(len%4!=0){
-            padding=4-(len%4);
+        int padding = 0;
+        if((len % 4) != 0){
+            padding = 4 - (len%4);
         }
-        total_length+=len+padding;
-        corrected_args[k]= (char *) kmalloc(sizeof(char)*(len+padding));
+        total_length += len + padding;
+        corrected_args[k] = (char *) kmalloc(sizeof(char)*(len+padding));
         err = copyin((const_userptr_t) args[k],corrected_args[k],len);
         if(err){
             return err;
         }
 
-        for(int j=len;j<len+padding;j++){
-            *(corrected_args[k]+j)= '\0';
+        for(int j = len; j< len+padding; j++){
+            *(corrected_args[k] + j) = '\0';
         }
     }
-    total_length+=((total+1)*4);
+    total_length += ((total + 1) * 4);
     // corrected_args[i] = (char *) kmalloc(sizeof(char) * PATH_MAX);
         
     //     err = copyinstr((const_userptr_t) program_name, kernel_program_name, PATH_MAX, &actual);
@@ -222,7 +222,7 @@ sys_execv(const char *program_name, char **args){
     //         return err; 
     //     }
     //Null terminated
-    corrected_args[total]=NULL;
+    corrected_args[total] = NULL;
 
 
 
@@ -265,7 +265,7 @@ sys_execv(const char *program_name, char **args){
     }
     int pointers_length = 4*(total+1);
     stackptr -= total_length;
-    int offset = stackptr+pointers_length;
+    int offset = stackptr + pointers_length;
 
     err = copyout(corrected_args,(userptr_t)offset, sizeof(corrected_args));
     if (err) {
