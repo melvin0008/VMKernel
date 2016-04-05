cacb0197ee652ab94610b6eed82a92a1c2e79ecf
Melvin <melvin0008@gmail.com>
2016-04-04 21:14:06 -0400

    working asst2 140/150

--- kern/syscall/proc_syscall.c
+++ kern/syscall/proc_syscall.c
@@ -302,7 +302,7 @@ sys_execv(const char *program_name, char **args){
     copyout( &temp,(void *) temp_stackptr, 4);
 
     // for(i=0;i<total;i++){
-    //     kfree(corrected_args);
+    //     kfree(*(corrected_args+i));
     // }
     // as_deactivate();
     // as_destroy(as);
