9bed432150f0f2ff226b6d3064215d54358f6c23
Melvin <melvin0008@gmail.com>
2016-04-07 01:23:57 -0400

    implemented alloc k pages . Not sure.

--- kern/vm/vm.c
+++ kern/vm/vm.c
@@ -3,22 +3,27 @@
 #include <types.h>
 #include <spinlock.h>
 #include <vm.h>
+#include <current.h>
+#include <spinlock.h>
+#include <cpu.h>
 
-
-
+static struct spinlock stealmem_lock = SPINLOCK_INITIALIZER;
+static uint32_t total_num_pages;
+static paddr_t free_address;
+static bool is_bootstrapped = false;
 void 
 init_coremap(){
+    spinlock_init(&stealmem_lock);
     paddr_t last_address = ram_getsize();
     paddr_t first_address = ram_getfirstfree();
-    paddr_t free_address;
     uint32_t index = 0;
-    uint32_t total_num_pages = (last_address - first_address) / PAGE_SIZE;
+    total_num_pages = (last_address - first_address) / PAGE_SIZE;
 
     coremap = (struct coremap_entry*) PADDR_TO_KVADDR(first_address);
     free_address = first_address + total_num_pages * (sizeof(struct coremap_entry));
 
     // Iterate all kernel entries
-    for(index = 0; index < (free_address / PAGE_SIZE); index += 1 ){
+    for(index = 0; index < (free_address / PAGE_SIZE); index ++ ){
         struct coremap_entry new_cmap_entry;
         // Init fields
         new_cmap_entry.is_fixed = true;
@@ -31,7 +36,7 @@ init_coremap(){
         coremap[index] = new_cmap_entry;
     }
 
-    for(index = (free_address / PAGE_SIZE); index < (last_address / PAGE_SIZE); index += 1 ){
+    for(index = (free_address / PAGE_SIZE); index < (last_address / PAGE_SIZE); index ++ ){
         struct coremap_entry new_cmap_entry;
         // Init fields
         new_cmap_entry.is_fixed = false;
@@ -43,13 +48,28 @@ init_coremap(){
 
         coremap[index] = new_cmap_entry;
     }
-
+    is_bootstrapped =true;
 }
 
 void
 vm_bootstrap(void){
 
 };
+
+static
+paddr_t
+getppages(unsigned long npages)
+{
+    paddr_t addr;
+
+    spinlock_acquire(&stealmem_lock);
+
+    addr = ram_stealmem(npages);
+
+    spinlock_release(&stealmem_lock);
+    return addr;
+}
+
 
 int
 vm_fault(int faulttype, vaddr_t faultaddress){
@@ -60,8 +80,66 @@ vm_fault(int faulttype, vaddr_t faultaddress){
 
 vaddr_t
 alloc_kpages(unsigned npages){
-    (void) npages;
-    return 0;
+    paddr_t p;
+    if(!is_bootstrapped){
+        p = getppages(npages);
+    }
+    else{
+        KASSERT(npages>0);
+        // if(npages>1){
+            //Multiple Pages
+            uint32_t start_page = 0;
+            for(uint32_t i = 0; i<total_num_pages; i++ ){
+                bool found_section = false;
+                uint32_t start_page = i;
+                if( i+npages> total_num_pages){
+                    return 0;
+                }
+                for(uint32_t j = i; j < i+npages; j++ ){
+                    if(!coremap[i].is_fixed && (coremap[i].is_free || coremap[i].is_clean || coremap[i].is_dirty)){
+                        found_section = true;
+                        i++;
+                    }
+                    else{
+                        found_section = false;
+                        break;
+                    }
+                }
+                if(i>=total_num_pages){
+                    return 0;
+                }
+                if(found_section){
+                    coremap[i].is_fixed = true;
+                    coremap[i].is_free  = false;
+                    coremap[i].is_dirty = false;
+                    coremap[i].is_clean = false;
+                    coremap[i].chunk_size = i - start_page;
+                    break;
+                }
+                p = start_page * PAGE_SIZE;
+            }
+            for (uint32_t i = start_page+1; i < npages+start_page; i++){
+                coremap[i].is_fixed = true;
+                coremap[i].is_free  = false;
+                coremap[i].is_dirty = false;
+                coremap[i].chunk_size = -1;
+                coremap[i].is_clean = false;
+            }
+        // }
+        // else{
+        //     //Single Page
+        //     for(int i = (free_address/PAGE_SIZE); i<total_num_pages; i++){
+        //         if(!coremap[i].is_fixed && (coremap[i].is_free || coremap[i].is_clean || coremap[i].is_dirty)){
+        //             coremap[i].is_fixed = true;
+        //             coremap[i].is_free  = false;
+        //             coremap[i].is_dirty = false;
+        //             coremap[i].is_clean = false;
+        //             coremap[i].chunk_size = 1;
+        //         }
+        //     }
+        // }
+    }
+    return PADDR_TO_KVADDR(p);
 };
 
 void
